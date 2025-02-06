dc44e5540fb8805e17d16be4a581c9dff27fb905
simone <simonegauli@gmail.com>
2025-02-06 19:54:02 +0100

    add empty test

---
+++ packages/express-status-monitor/__tests__/status-monitor.test.ts
@@ -0,0 +1,8 @@
+import { describe, it } from '@jest/globals';
+
+describe('NOTEST', () => {
+  it('NOTEST', () => {
+    // eslint-disable-next-line no-console
+    console.log('NOTEST');
+  });
+});
